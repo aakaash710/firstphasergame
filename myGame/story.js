eb17f75d7368cc669400eb9ec417602b113bfffa
Aakaash Narayannan <anarayannan.ra@mysummitps.org>
2016-12-02 20:45:41 +0000

    update

--- myGame/story.js
+++ myGame/story.js
@@ -14,17 +14,22 @@ game_state.story.prototype = {
 
     create: function() {
         game.add.sprite(0, 0, 'sky');
-        this.text = game.add.text(200, 200, 'Welcome to Pacman world \n try to collect all the stars \n you have to get 150 points', {
+        this.text = game.add.text(200, 200, 'Welcome to Pacman world \n try to collect all the stars \n you have to get 150 points \n wait 10 seconds or press the up arrow key \n to continue ', {
             fontSize: '32px',
             fill: 'green'
         });
         setTimeout(function(){
             game.state.start('main')
         }, 10000)
+        
+        this.cursors = game.input.keyboard.createCursorKeys();
     },
     update: function() {
 
-
+ if (this.cursors.up.isDown) {
+     game.state.start('main')
+ }
+ 
 }
 
 
